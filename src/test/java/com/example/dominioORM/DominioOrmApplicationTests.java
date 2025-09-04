79b8f41dac46eec2750133a05f6b505a8a1522f4
Wagner Vinicius <wagnervsantos@hotmail.com>
2025-09-04 01:13:49 -0300

    Entities criada

---
+++ src/test/java/com/example/dominioORM/DominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
